efcae754403c79e2c0594fc7eb6dee18a2ccc944
techygrrrl <techygrrrl.streams@gmail.com>
2022-08-29 21:41:52 -0700

    clean up Bank Account

--- kotlin/bank-account/src/main/kotlin/BankAccount.kt
+++ kotlin/bank-account/src/main/kotlin/BankAccount.kt
@@ -1,8 +1,4 @@
-import java.util.concurrent.atomic.AtomicInteger
-import java.util.concurrent.atomic.AtomicReference
 import java.util.concurrent.locks.ReentrantLock
-import kotlin.properties.ReadWriteProperty
-import kotlin.reflect.KProperty
 
 class BankAccount {
     private val lock = ReentrantLock(true)
@@ -13,9 +9,6 @@ class BankAccount {
     }
 
     private var _balance: Int = 0
-//    private var _balance: Int by atomic(0)
-//    private var _balance = AtomicInteger(0)
-
 
     private var state: State = State.OPEN
 
@@ -24,9 +17,6 @@ class BankAccount {
         CLOSED
     }
 
-    /**
-     * Add or remove money depending on the positive/negative'ness
-     */
     fun adjustBalance(amount: Long){
         if (state == State.CLOSED) throw IllegalStateException()
 
@@ -35,36 +25,10 @@ class BankAccount {
         val newBalance: Int = previousBalance + amount.toInt()
         _balance = newBalance
         lock.unlock()
-
-        // by atomic delegate
-//        val previousBalance = balance
-//        val newBalance: Int = previousBalance + amount.toInt()
-//        _balance = newBalance
-
-        // Re-entrant lock
-
-
-//        val newBalance = _balance.addAndGet(amount.toInt())
-//        _balance.set(newBalance.toInt())
-//        balance += amount.toInt()
     }
 
     fun close() {
         state = State.CLOSED
-    }
-}
-
-
-private fun <T>atomic(tIn: T): ReadWriteProperty<Any?, T> {
-    return object : ReadWriteProperty<Any?, T> {
-        val t = AtomicReference<T>(tIn)
-        override fun getValue(thisRef: Any?, property: KProperty<*>): T {
-            return t.get()
-        }
-
-        override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
-            t.set(value)
-        }
     }
 }
 
