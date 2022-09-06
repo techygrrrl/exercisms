f7a7a9172c43601670fb0bfe41b3f7ec8ebd4145
techygrrrl <techygrrrl@proton.me>
2022-09-05 20:00:55 -0700

    solution kindergarten garden

---
+++ kotlin/kindergarten-garden/src/main/kotlin/KindergartenGarden.kt
@@ -0,0 +1,36 @@
+class KindergartenGarden(private val diagram: String) {
+
+    private val rows = diagram.split("\n")
+
+    private val students = listOf(
+        "Alice", "Bob", "Charlie",
+        "David", "Eve", "Fred",
+        "Ginny", "Harriet", "Ileana",
+        "Joseph", "Kincaid", "Larry",
+    )
+
+    fun getPlantsOfStudent(student: String): List<String> {
+        val studentIndex = students.indexOf(student)
+        val firstPlantIndex = if (studentIndex == 0) {
+            0
+        } else {
+            studentIndex * 2
+        }
+
+        return rows
+            .flatMap { row ->
+                listOf(
+                    getPlantName(row[firstPlantIndex]),
+                    getPlantName(row[firstPlantIndex + 1]),
+                )
+            }
+    }
+
+    private fun getPlantName(letter: Char): String =
+        mapOf(
+            'C' to "clover",
+            'G' to "grass",
+            'R' to "radishes",
+            'V' to "violets"
+        )[letter]!!
+}
